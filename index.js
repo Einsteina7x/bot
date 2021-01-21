8eedb1e27208d745595434a6a0170d328b2b13f4
Nasgorest <77262726+Nasgorest@users.noreply.github.com>
2021-01-21 16:28:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -272,7 +272,6 @@ async function starts() {
     case 'owner':
     case 'creator':
      client.sendMessage(from, {displayname: "Jeff", vcard: vcard}, MessageType.contact, { quoted: mek})
-     client.sendMessage(from, 'Nih nomor ownerku kak, save ya kak nanti di save balik',MessageType.text, { quoted: mek} )
      break
 
     case 'notif':
